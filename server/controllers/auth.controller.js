4d6b67d4002486416eaca239c13f56f5042fbe76
Sachin Singh <sachinsingh9272@gmail.com>
2025-01-30 23:13:01 +0530

    Created Login route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -22,3 +22,32 @@ export const Signup = async (req, res, next) => {
         next(error);
     }
 }
+
+// Sign In API 
+export const Signin = async(req,res,next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid Credential'));
+        }
+        const token = jwt.sign({id:validUser._id, isAdmin: validUser.isAdmin}, process.env.JWT_SECRET);
+
+        const {password: pass, ...rest} =validUser._doc;
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {Signup} from '../controllers/auth.controller.js'
+import { Signup, Signin } from '../controllers/auth.controller.js'
 
 const router = express.Router();
 
 router.post('/signup', Signup)
+router.post('/login', Signin);
 
 export default router;
