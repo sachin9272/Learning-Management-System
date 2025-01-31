782c130548052b85398220dcef192a38163b1933
Sachin Singh <sachinsingh9272@gmail.com>
2025-01-31 23:28:53 +0530

    Implemented Firebase route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { Signup, Signin } from '../controllers/auth.controller.js'
+import { Signup, Signin, Google } from '../controllers/auth.controller.js'
 
 const router = express.Router();
 
 router.post('/signup', Signup)
 router.post('/login', Signin);
+router.post('/google', Google);
 
 export default router;
