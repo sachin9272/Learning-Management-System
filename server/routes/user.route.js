ca64542e2ffa3a81e92f580bbf2a6ffa4d607ac9
Sachin Singh <sachinsingh9272@gmail.com>
2025-02-02 23:20:57 +0530

    Implemented update user route

---
+++ server/routes/user.route.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import { updateUser } from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.put('/update/:userId',verifyToken, updateUser);
