67c5d9ab8656ae716ec4595f8a6a2316e799b967
Sachin Singh <sachinsingh9272@gmail.com>
2025-02-08 23:51:22 +0530

    Implemented get User route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { updateUser, deleteUser, signout } from '../controllers/user.controller.js';
+import { updateUser, deleteUser, signout, getUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.put('/update/:userId',verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser)
 
 
 export default router;
