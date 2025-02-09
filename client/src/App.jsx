4f62317e9236ca524c525e086ca449838bd34542
Sachin Singh <sachinsingh9272@gmail.com>
2025-02-09 23:50:37 +0530

    Added routes in Main file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 const App = () => {
   return (
-    <div className="">
-      
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>}/>
+        <Route path='/about' element={<h1>About</h1>}/>
+        <Route path='/contact' element={<h1>Contact</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
